ffab15889b09a96f52d8d84b3fc4351e56ee0b26
alek3y <alek3y@protonmail.com>
2021-08-17 17:51:06 +0200

    Make all Cursor fields public

--- src/buffer/cursor.rs
+++ src/buffer/cursor.rs
@@ -2,20 +2,12 @@ use super::position::Position;
 
 #[derive(Debug)]
 pub struct Cursor {
-	position: Position,
-	offset: usize
+	pub position: Position,
+	pub offset: usize
 }
 
 impl Cursor {
 	pub fn new(position: Position, offset: usize) -> Self {
 		Self {position, offset}
-	}
-
-	pub fn position(&self) -> Position {
-		self.position
-	}
-
-	pub fn offset(&self) -> usize {
-		self.offset
 	}
 }
