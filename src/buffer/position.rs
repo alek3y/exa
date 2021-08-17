4b95a7ed91d4d9467cb0184a1456265896cbae34
alek3y <alek3y@protonmail.com>
2021-08-17 17:52:50 +0200

    Add compare methods to Position

--- src/buffer/position.rs
+++ src/buffer/position.rs
@@ -1,3 +1,5 @@
+use std::cmp::{PartialOrd, Ordering};
+
 #[derive(PartialEq, Copy, Clone, Debug)]
 pub struct Position {
 	pub line: usize,
@@ -7,5 +9,23 @@ pub struct Position {
 impl Position {
 	pub fn new(line: usize, column: usize) -> Self {
 		Self {line, column}
+	}
+}
+
+impl PartialOrd for Position {
+	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+		if other == self {
+			return Some(Ordering::Equal);
+		}
+
+		if other.line < self.line {
+			return Some(Ordering::Greater);
+		}
+
+		if other.line == self.line && other.column < self.column {
+			return Some(Ordering::Greater);
+		}
+
+		return Some(Ordering::Less);
 	}
 }
