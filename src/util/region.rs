706cf82ebce1a313faa2899541fbfb239a008943
alek3y <alek3y@protonmail.com>
2021-08-19 16:14:34 +0200

    Name each corner on Region

--- src/util/region.rs
+++ src/util/region.rs
@@ -2,11 +2,16 @@ use super::position::Position;
 
 #[derive(Debug)]
 pub struct Region {
-	pub corners: (Position, Position)
+	pub top_left: Position,
+	pub bottom_right: Position
 }
 
 impl Region {
+	pub fn new(top_left: Position, bottom_right: Position) -> Self {
+		Self {top_left, bottom_right}
+	}
+
 	pub fn contains(&self, position: Position) -> bool {
-		position >= self.corners.0 && position <= self.corners.1
+		position >= self.top_left && position <= self.bottom_right
 	}
 }
