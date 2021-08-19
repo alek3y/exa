44169c51777cd0bcfd07478f84df9a9d6ae4e7ab
alek3y <alek3y@protonmail.com>
2021-08-19 16:50:26 +0200

    Add Border for drawing the boundaries of Pane

--- src/interface/pane/mod.rs
+++ src/interface/pane/mod.rs
@@ -1,10 +1,19 @@
 use super::{region::Region, Interface};
 
+pub mod border;
+use border::Border;
+
 #[derive(Debug)]
 pub struct Pane {
+	border: Border,
 	region: Region,
 	rows: Vec<String>,
 	line_offset: usize
 }
 
+impl Pane {
+	pub fn new(border: Border, region: Region, line_offset: usize) -> Self {
+		Self {border, region, rows: Vec::new(), line_offset}
+	}
+}
 // TODO: impl Interface for Pane
